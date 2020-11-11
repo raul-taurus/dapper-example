fcc7f75687a148a5bbc611f6ba1ef30760e4c4a2
Raul <raul.guoping@gmail.com>
2020-11-11 21:17:04 +0800

    Add dapper mysql example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dapper examples
+
+## Start a mysql instance
+
+```sh
+docker run --name some-mariadb -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mariadb
+```
---
+++ src/mysql-dapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapper;
+using MySqlConnector;
+
+namespace mysql_dapper
+{
+    class Program
+    {
+        const string mysql_password = "my-secret-pw";
+
+        static async Task Main(string[] args)
+        {
+            using (var connection = new MySqlConnection($"Server=172.17.0.2;User ID=root;Password={mysql_password};Database=mysql"))
+            {
+                var date = await connection.QueryFirstAsync<MySqlDatabaseTime>("SELECT UTC_TIMESTAMP() AS UtcNow, NOW()+1 AS Now;");
+                Console.WriteLine($"{date.Now} - {date.UtcNow}");
+            }
+        }
+    }
+
+    public class MySqlDatabaseTime
+    {
+        public long Now { get; set; }
+        public DateTimeOffset UtcNow { get; set; }
+    }
+}
